4a46fabc3937e6f5d607a54c78add1eda3d06d34
lecio19 <alex.macri01@gmail.com>
2024-11-29 16:30:25 +0100

    add header

---
+++ react-use-state/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import style from './Header.module.css'
+
+export default function Header() {
+    return (
+        <header>
+
+            <h1 className={style.header}>Learn Web Development</h1>
+
+        </header>
+    )
+}
