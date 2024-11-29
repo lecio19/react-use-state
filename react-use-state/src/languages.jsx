91222df95c8e643627c9378b0b694c6104e2d85d
lecio19 <alex.macri01@gmail.com>
2024-11-29 15:50:13 +0100

    add  array

---
+++ react-use-state/src/languages.jsx
@@ -0,0 +1,32 @@
+const languages = [
+    {
+      id: 1,
+      title: "HTML",
+      description: "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web."
+    },
+    {
+      id: 2,
+      title: "CSS",
+      description: "CSS è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML."
+    },
+    {
+      id: 3,
+      title: "JavaScript",
+      description: "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web."
+    },
+    {
+      id: 4,
+      title: "Node.js",
+      description: "Node.js è un runtime system open source multipiattaforma orientato agli eventi per l'esecuzione di codice JavaScript."
+    },
+    {
+      id: 5,
+      title: "Express",
+      description: "Framework backend minimalista e veloce che offre funzionalità e strumenti solidi per lo sviluppo diapplicazioni di backend scalabili."
+    },
+    {
+      id: 6,
+      title: "ReactJS",
+      description: "ReactJS è una libreria JavaScript per costruire interfacce utente"
+    }
+  ];
